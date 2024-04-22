37ce8b25934501feaeb6c022700819b4c3348745
Dinesh <Dineshchauhan416pd@gmail.com>
2024-04-22 09:22:30 +0530

    Components done and Axios setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import UserCard from "./components/UserCard"
+
 function App() {
 
+  const [users, setUsers] =  ([]);
+
+  useEffect(()=>{
+
+  },[])
+
   return (
-    <>
-      <h1 className="">Welcome to Axios Tutorial</h1>
-    </>
+    <div className="max-w-7xl mx-auto py-2">
+      <h1 className="text-2xl my-4 font-medium text-center">Welcome to Axios Tutorial</h1>
+      <div className="flex flex-row flex-wrap gap-2 justify-evenly items-center">
+        {users &&  users.map((user) => (
+          <div key={user.id} className="">
+            <UserCard name="" city="" email="" contact="" website="" />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function UserCard({name= "Veer", email="veer@gmail.com", website="veer.info", contact="635482xxx", city="sirohi"}) {
+  return (
+    <div className='max-w-[320px] w-full bg-gray-500/30 p-2 rounded-xl hover:border-[1px] border-white/40 transition-all duration-200' >
+        <div className='flex flex-col items-normal text-center justify-center'>
+            <h1 className='font-semibold text-xl'>{name}</h1>
+            <div className=''>
+                <h2 className=''>email:    {email} </h2>
+                <h2 className=''>contact: {contact}</h2>
+            </div>
+            <div>
+                <p className=''>city: {city}</p>
+                <p className=''>website: {website}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UserCard
