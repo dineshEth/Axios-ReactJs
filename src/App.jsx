13967ab4245018615658630aed4b09cd30081f2c
Dinesh <Dineshchauhan416pd@gmail.com>
2024-04-22 11:48:30 +0530

    Project Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 function App() {
 
   const [users, setUsers] =  useState([]);
-  const [Loader, setLoader] = useState(true)
+  const [Loader, setLoader] = useState(true);
 
   useEffect(()=>{
     axios
@@ -22,9 +22,9 @@ function App() {
   return (
     <>
     {Loader ? <h1 className="text-2xl flex flex-row justify-center items-center h-screen w-screen font-bold text-white">Loading...</h1> : (
-      <div className="max-w-7xl mx-auto py-2">
-      <h1 className="text-2xl my-4 font-medium text-center">Welcome to Axios Tutorial</h1>
-      <div className="flex flex-row flex-wrap gap-2 justify-evenly items-center">
+      <div className="max-w-7xl mx-auto p-2">
+      <h1 className="text-4xl my-4 font-bold text-center">Welcome to Axios Tutorial</h1>
+      <div className="flex flex-row my-8 flex-wrap gap-2 justify-evenly items-center">
         {users &&  users.map((user) => (
           <div key={user.id} className="max-w-[320px] w-full">
             <UserCard name={user.name} city={user.address.city} email={user.email} contact={user.phone} website={user.website} />
